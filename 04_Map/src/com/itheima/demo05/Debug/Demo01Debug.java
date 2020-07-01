cd469522a21a44d26f2288731e38f13589d1875b
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-07-01 19:44:32 +0200

    add: 04_Map and debug

--- 04_Map/src/com/itheima/demo05/Debug/Demo01Debug.java
+++ 04_Map/src/com/itheima/demo05/Debug/Demo01Debug.java
@@ -16,14 +16,14 @@ package com.itheima.demo05.Debug;
  */
 public class Demo01Debug {
     public static void main(String[] args) {
-        /*int a = 10;
+        int a = 10;
         int b = 20;
         int sum = a + b;
-        System.out.println(sum);*/
+        System.out.println(sum);
 
-        /*for (int i = 0; i <3 ; i++) {
+        for (int i = 0; i <3 ; i++) {
             System.out.println(i);
-        }*/
+        }
 
         print();
 
